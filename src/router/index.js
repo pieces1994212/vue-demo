0a9846d39cea19530102ab677ef88d676542b1c8
piecesXu <pieces1994212@163.com>
2018-09-30 16:02:15 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import Login from '../components/Login'
 import VueRouter from 'vue-router'
 import N404 from '../components/404'
 import Vue from 'vue'
-// 启用路由
+
 Vue.use(VueRouter)
+
 const routes = [{
   path: '/',
   name: 'login',
@@ -14,7 +15,6 @@ const routes = [{
   component: N404
 }]
 
-// 获取路径
 const router = new VueRouter({
   routes,
   mode: 'hash'
