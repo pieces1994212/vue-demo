a894b03c8e99733d9d1ff9eaa56e5cfdf250c7fd
piecesXu <pieces1994212@163.com>
2018-11-05 17:18:07 +0800

    优化主体内404

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,3 +1,5 @@
+import N404 from '../components/404'
+
 export const initMenu = (store, router, routes) => {
   let fmRouter = fomatRoutes(routes)
   let extendsRoutes = [{
@@ -5,9 +7,6 @@ export const initMenu = (store, router, routes) => {
     name: 'home',
     component: (resolve) => require(['../components/Home'], resolve),
     children: fmRouter
-  }, {
-    path: '*',
-    redirect: '/404'
   }]
   router.addRoutes(extendsRoutes)
   store.commit({
@@ -26,5 +25,12 @@ export const fomatRoutes = (routes) => {
     }
     fmRoutes.push(fmRouter)
   })
+  // 添加home子路由的404
+  let route404 = {
+    name: 'home404',
+    path: '*',
+    component: N404
+  }
+  fmRoutes.push(route404)
   return fmRoutes
 }
